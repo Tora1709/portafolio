4cf844aa2ad5aeb1a3123493c0119a881baff146
Kevin <kvegac@ucenfotec.ac.cr>
2018-02-27 17:18:33 -0600

    ajustes

--- public/route.js
+++ public/route.js
@@ -11,8 +11,8 @@
 
 		.state('landing',{
 			url: '/Home',
-			templateUrl: './components/landing/landing.html',
-			css: './css/style.gallery.css',
+			templateUrl: './components/home/home.html',
+			css: './css/agency.min.css',
 			// resolve: {
 			// 	load: ['$ocLazyLoad', function($ocLazyLoad){
 			// 		return $ocLazyLoad.load('./components/landing/landing.controller.js')
